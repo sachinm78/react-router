5311be84e05e336222ed079e3a727f9077335882
sachinm78 <sachinm78@gmail.com>
2020-01-02 13:49:14 -0600

    created About and Home components; rendered using 3 different methods in App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,37 @@
 import React from "react"
 import {Link, Switch, Route} from "react-router-dom"
 
+import Home from "./Home"
+import About from "./About"
+
 function App() {    
     return (
         <div>
             <Link to="/">Home</Link>
             <Link to="/about">About</Link>
             
+            {/* Newest way using children */}
             <Switch>
-                <Route exact path="/" render={() => <h1>Home Page!</h1>} />
-                <Route path="/about" render={() => <h1>About Page!</h1>} />
+                <Route exact path="/"><Home /></Route>
+                <Route path="/about"><About /></Route>
             </Switch>
         </div>
     )
 }
 
 export default App
+
+// Old way with render
+/* <Switch>
+    <Route exact path="/" render={() => <Home />} />
+    <Route path="/about" render={() => <About />} />
+</Switch> */
+
+// 2nd option with component
+/* <Switch>
+    <Route exact path="/" component={Home} />
+    <Route path="/about" component={About} />
+</Switch> */
+
+
+
