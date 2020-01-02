042e587ada8b481140ae5de7ec5bec1293144dbe
sachinm78 <sachinm78@gmail.com>
2020-01-02 13:39:26 -0600

    set up switch paths in app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from "react"
-import {Link} from "react-router-dom"
+import {Link, Switch, Route} from "react-router-dom"
 
 function App() {    
     return (
         <div>
             <Link to="/">Home</Link>
             <Link to="/about">About</Link>
+            
+            <Switch>
+                <Route exact path="/" render={() => <h1>Home Page!</h1>} />
+                <Route path="/about" render={() => <h1>About Page!</h1>} />
+            </Switch>
         </div>
     )
 }
