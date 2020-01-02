0be50a55e71a81203b38c81bf66369a9966975d4
sachinm78 <sachinm78@gmail.com>
2020-01-02 14:01:19 -0600

    created Contact component and added nav link and component to App.js

--- src/App.js
+++ src/App.js
@@ -3,17 +3,20 @@ import {Link, Switch, Route} from "react-router-dom"
 
 import Home from "./Home"
 import About from "./About"
+import Contact from "./Contact"
 
 function App() {    
     return (
         <div>
             <Link to="/">Home</Link>
             <Link to="/about">About</Link>
+            <Link to="/contact">Contact</Link>
             
             {/* Newest way using children */}
             <Switch>
                 <Route exact path="/"><Home /></Route>
                 <Route path="/about"><About /></Route>
+                <Route path="/contact"><Contact /></Route>
             </Switch>
         </div>
     )
